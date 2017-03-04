0fb36a8195e438dbeddf96b655f6ea4346742950
Snuupy <snuupy@[-removethis-]snusite.com>
2017-03-04 04:29:25 -0800

    sockets for starting game

--- index.ios.js
+++ index.ios.js
@@ -13,6 +13,7 @@ import {
   MapView,
   Image
 } from 'react-native'
+import SocketIOClient from 'socket.io-client';
 
 // This is the root view
 var Connect4 = React.createClass({
@@ -31,6 +32,9 @@ var Connect4 = React.createClass({
 
 var Interface= React.createClass({
   getInitialState() {
+    this.setState({
+      socket: this.props.socket
+    });
     // const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     //
     // fetch('https://hohoho-backend.herokuapp.com/messages', {
@@ -47,7 +51,7 @@ var Interface= React.createClass({
     //   })
     // })
     return{
-      initialRow: ["🐳","🐼","🐸","🐙","🐨","🐤","🐩"],
+      initialRow: [1, 2, 3, 4, 5, 6, 7],
       dataSource: [["0","0","0","0","0","0","0"],
       ["0","0","0","0","0","0","0"],
       ["0","0","0","0","0","0","0"],
@@ -57,18 +61,24 @@ var Interface= React.createClass({
       ["0","0","0","0","0","0","0"]]
     }
   },
-
+  press(row){
+    alert(row);
+  },
   render(){
     return (
 
       <View style={styles.container}>
       <Image style={styles.backgroundImage} source={{uri: 'https://s-media-cache-ak0.pinimg.com/originals/ac/99/13/ac991305df5fb4c1cdd53bf7f5535a4e.gif'}}/>
       <Text style={styles.textBig}> Current Turn:</Text>
-      <TouchableOpacity>
-      <Text>{this.state.initialRow.map((panda)=>
-      <Text style={styles.roundbutton}>{panda}</Text>)}
-      </Text>
-      </TouchableOpacity>
+
+      <View style={{flexDirection: "row"}}>
+        {this.state.initialRow.map((row)=>
+          <TouchableOpacity onPress={this.press.bind(this, row)}>
+            <Text style={styles.roundbutton}>{row}</Text>
+          </TouchableOpacity>
+        )}
+      </View>
+
       {this.state.dataSource.map((row)=>
       <Text style={styles.grid}>{row.map((item) =>
       <Text>{item}</Text>)}
@@ -96,10 +106,9 @@ var Interface= React.createClass({
 
 var Games = React.createClass({
   getInitialState() {
-
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
-    fetch('https://hohoho-backend.herokuapp.com/users', {
+    fetch('http://localhost:3000/users', {
       method: 'GET',
       headers: {
         "Content-Type": "application/json"
@@ -109,28 +118,90 @@ var Games = React.createClass({
       //   password: this.state.password,
       // })
     })
-    .then((response) => response.json())
+    .then((response) => {
+
+      return response.json()
+    })
     .then((responseJson) => {
-      console.log(responseJson)
+
       this.setState({
         dataSource: ds.cloneWithRows(responseJson.users)
       })
     })
+    .catch((err) => {
+
+      console.log('error', err);
+    });
     return{
       dataSource: ds.cloneWithRows([]),
     }
   },
+  componentDidMount(){
+    var count = 0;
+    this.setState({
+      socket: this.props.socket
+    }, () => { //callback when state is set and socket is ready to be used, set up listeners here
+      var self = this;
+
+      this.state.socket.on('newUserAdded', function(data){
+        count++;
+        if (count === 2){
+          //start game
+          self.state.socket.emit('startGame', {});
+        }
+      });
+      this.state.socket.on('startGame', function(data){
+        gameStarted();
+      });
+      this.state.socket.on('start', function(data){
+        gameStarted();
+      });
+
+      var gameStarted = () => {
+        //go to interface page here
+      }
+
+    })
+  },
+  press(username, id){
+    var currentUsername;
+    var currentId;
+
+    AsyncStorage.getItem('user')
+    .then(result => {
+      var parsedResult = JSON.parse(result);
+      console.log(parsedResult.username, parsedResult.id);
+      currentUsername = parsedResult.username;
+      currentId = parsedResult.id;
+
+      this.state.socket.emit('addPlayer', {
+        username: parsedResult.username,
+        id: parsedResult.id
+      });
+      this.state.socket.emit('addPlayer', {
+        username: username,
+        id: id
+      }); //add both players, when both players are added start the game
 
 
+    })
+    .catch(err => console.log(err));
+  },
 
   render(){
     return (
       <View style={styles.container}>
-      <ListView
-      dataSource={this.state.dataSource}
-      renderRow={(rowData) => <View style={styles.container}>
-      <Text>{rowData.username} : {rowData.score}</Text>
-      </View>} />
+        <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) =>
+
+          <View style={styles.container}>
+            <TouchableOpacity onPress={this.press.bind(this, rowData.username, rowData._id)}>
+              <Text>{rowData.username}</Text>
+            </TouchableOpacity>
+          </View>}
+        />
+
       </View>
     )
   },
@@ -160,12 +231,10 @@ var Register = React.createClass({
     })
     .then((response) => response.json())
     .then((responseJson) => {
-      console.log('EYYY: ', responseJson);
       if(responseJson.success === true){
         console.log('Success!');
       }
 
-      console.log(this.state.username, this.state.password)
       this.props.navigator.pop()
     })
     /* do something with responseJson and go back to the Login view but
@@ -202,7 +271,8 @@ var Login = React.createClass({
   getInitialState () {
     return {
       username: '',
-      password: ''
+      password: '',
+      socket: this.props.socket
     }
   },
   interface(){
@@ -227,20 +297,27 @@ var Login = React.createClass({
     })
     .then((response) => response.json())
     .then((responseJson) => {
+
       this.props.navigator.push({
         component: Games,
-        title: "Games",
+        title: "User List",
         passProps: {
           username: this.state.username,
-          password: this.state.password
+          password: this.state.password,
+          id: responseJson.user._id,
+          socket: this.state.socket
         },
         rightButtonTitle: 'Interface',
-        onRightButtonPress: this.interface
+        onRightButtonPress: this.interface,
+        passProps: {
+          socket: this.state.socket
+        }
       })
-      console.log('storing ', this.state.username, this.state.password);
+
       AsyncStorage.setItem('user', JSON.stringify({
         username: this.state.username,
-        password: this.state.password
+        password: this.state.password,
+        id: responseJson.user._id
       }));
 
 
@@ -248,7 +325,7 @@ var Login = React.createClass({
     /* do something with responseJson and go back to the Login view but
     * make sure to check for responseJson.success! */
     .catch((err) => {
-      console.log('LOGIN ERROR BOOOI', err);
+      alert(err);
     });
   },
 
@@ -277,8 +354,15 @@ var Login = React.createClass({
 
 
 var FrontPage = React.createClass({
+
+  getInitialState: function() {
+  return {
+    socket: SocketIOClient('http://localhost:3000')
+  }
+},
+
   componentDidMount(){
-    console.log('eyyy1');
+
     AsyncStorage.getItem('user')
     .then(result => {
       var parsedResult = JSON.parse(result);
@@ -286,14 +370,14 @@ var FrontPage = React.createClass({
       var password = parsedResult.password;
       console.log(result, username, password);
       if (username && password) {
-        console.log('You got it mate!', this.props.navigator)
         this.props.navigator.push({
           component: Games,
           title: "Games",
           passProps: {
             username: username,
-            password: password
-          },
+            password: password,
+            socket: this.state.socket
+        },
           rightButtonTitle: 'Interface',
           onRightButtonPress: this.interface
         });
@@ -305,21 +389,30 @@ var FrontPage = React.createClass({
   interface(){
     this.props.navigator.push({
       component: Interface,
-      title: "Game in session"
+      title: "Game in session",
+      passProps: {
+        socket: this.state.socket
+      }
     });
   },
 
   login() {
     this.props.navigator.push({
       component: Login,
-      title: "Login"
+      title: "Login",
+      passProps: {
+        socket: this.state.socket
+      }
     });
   },
 
   register() {
     this.props.navigator.push({
       component: Register,
-      title: "Register"
+      title: "Register",
+      passProps: {
+        socket: this.state.socket
+      }
     });
   },
 
@@ -432,7 +525,7 @@ grid:{
 roundbutton:{
     backgroundColor: "transparent",
     borderColor: '#000',
-    fontSize: 20,
+    fontSize: 40,
     opacity: 0.75
 },
 
